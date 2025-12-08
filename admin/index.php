7208c8da7006d12cc169fc0c3a26685c47e7f65e
jannatun-nahar <nahar22205101281@diu.edu.bd>
2025-12-08 19:09:23 +0000

    Index in php

--- admin/index.php
+++ admin/index.php
@@ -1,81 +1,62 @@
-<?php    
-    require('./inc/essentials.php');
-    require('./inc/connection.php');
+<?php
+session_start();
+include './inc/connection.php';
 
-    session_start();
-    if((isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){
-      redirect('dashboard.php'); 
+if(isset($_SESSION['adminLogin'])){
+    header("Location: dashboard.php");
+    exit;
+}
 
+$error = "";
+
+if(isset($_POST['login'])){
+    $admin_name = $_POST['admin_name'];
+    $admin_pass = $_POST['admin_pass'];
+
+    $sql = "SELECT * FROM admin_login 
+            WHERE admin_name='$admin_name' AND admin_pass='$admin_pass'";
+
+    $res = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($res) == 1){
+        $row = mysqli_fetch_assoc($res);
+
+        $_SESSION['adminLogin'] = true;
+        $_SESSION['adminID'] = $row['id'];
+
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $error = "Invalid username or password!";
     }
-
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php require('./inc/links.php') ?>
-    <style>
-    div.login-form {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 400px;
-    }
-    </style>
+    <title>Admin Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body class="bg-light">
 
-    <div class="login-form text-center rounded bg-white shadow overflow-hidden">
-        <form action="" method="POST">
-            <h4 class="bg-dark text-white py-3">Admin Login Panel</h4>
-            <div class="p-4">
-                <div class="mb-3">
-                    <input name="admin_name" required type="text" class="form-control shadow-none"
-                        placeholder="Admin Name">
-                </div>
-                <div class="mb-4">
-                    <input type="password" required name="admin_pass" class="form-control shadow-none"
-                        placeholder="Password">
-                </div>
-                <button name="login" type="submit" class="btn text-white custom-bg shadow-none">LOGIN</button>
-            </div>
+    <div class="login-form bg-white p-4 shadow-lg" style="width:400px; margin:150px auto; border-radius:10px;">
+
+        <h3 class="text-center mb-4">Admin Login</h3>
+
+        <?php if($error){ echo "<div class='alert alert-danger'>$error</div>"; } ?>
+
+        <form method="POST">
+            <input type="text" name="admin_name" class="form-control mb-3" placeholder="Admin Name" required>
+
+            <input type="password" name="admin_pass" class="form-control mb-3" placeholder="Password" required>
+
+            <button name="login" class="btn btn-primary w-100">Login</button>
         </form>
     </div>
-    <?php 
-        if(isset($_POST['login'])){
-            $frm_data = filteration($_POST);
-            // echo"<h1>$frm_data[admin_name]</h1>";
-            // echo"<h1>$frm_data[admin_pass]</h1>";
 
-            $query = "SELECT *FROM `admin_login` WHERE `admin_name`=? AND `admin_pass`=? ";
-            $values = [$frm_data['admin_name'],$frm_data['admin_pass']];
-
-            $res = select($query, $values,'ss');
-            // print_r($res);
-
-            if($res->num_rows == 1){
-                // echo "got user";
-                $row = mysqli_fetch_assoc($res);
-            
-                $_SESSION['adminLogin'] = true;
-                $SESSION['adminID'] = $row['id'];
-                redirect('dashboard.php');
-            }else{
-                alert('error','Login failed - Invalid Credentials!');
-              
-            }
-        }
-
-  
-    
-    ?>
-    <?php require('./inc/script.php') ?>
 </body>
 
 </html>
