98437c813b91687d8e6cfcd8cc445a9dce9aeea2
Mohammad Faisal <83537344+faisal1279@users.noreply.github.com>
2025-12-09 23:15:42 +0600

    Update rooms.php

--- rooms.php
+++ rooms.php
@@ -24,10 +24,11 @@ require('admin/inc/connection.php');
     <div class="container">
         <div class="row">
 
-            <!-- LEFT FILTER SECTION (STATIC as your design) -->
+
             <div class="col-lg-3 col-md-12 mb-lg-0 mb-4 px-lg-0">
                 <nav class="navbar navbar-expand-lg navbar-light bg-white rounded shadow">
                     <div class="container-fluid flex-lg-column align-items-stretch">
+
                         <h4 class="mt-2">Filters</h4>
 
                         <button class="navbar-toggler shadow-none" data-bs-toggle="collapse"
@@ -36,55 +37,79 @@ require('admin/inc/connection.php');
                         </button>
 
                         <div class="collapse navbar-collapse flex-column mt-2 align-items-stretch" id="filterDropdown">
-                            <div class="border bg-light p-3 rounded mb-3">
-                                <h5 class="mb-3">CHECK AVAILABILITY</h5>
-                                <label>Check-in</label>
-                                <input type="date" class="form-control mb-3">
 
-                                <label>Check-out</label>
-                                <input type="date" class="form-control">
-                            </div>
 
-                            <div class="border bg-light p-3 rounded mb-3">
-                                <h5 class="mb-3">FACILITIES</h5>
-                                <div><input type="checkbox" class="form-check-input me-1"> Wifi</div>
-                                <div><input type="checkbox" class="form-check-input me-1"> AC</div>
-                                <div><input type="checkbox" class="form-check-input me-1"> TV</div>
-                            </div>
+                            <form method="GET">
+                                <div class="border bg-light p-3 rounded mb-3">
+                                    <h5 class="mb-3">CHECK AVAILABILITY</h5>
+
+                                    <label>Check-in</label>
+                                    <input type="date" name="checkin" class="form-control mb-3"
+                                        value="<?= $_GET['checkin'] ?? ''; ?>">
+
+                                    <label>Check-out</label>
+                                    <input type="date" name="checkout" class="form-control mb-3"
+                                        value="<?= $_GET['checkout'] ?? ''; ?>">
+
+                                    <button type="submit" class="btn btn-dark w-100">Search</button>
+                                </div>
+                            </form>
+
                         </div>
                     </div>
                 </nav>
             </div>
 
-            <!-- RIGHT ROOM LIST (DYNAMIC FROM DATABASE) -->
+
+
             <div class="col-lg-9 col-md-12 px-4">
 
                 <?php
-                $rooms = mysqli_query($conn, "SELECT * FROM room_details ORDER BY id DESC");
+                
+                $checkin  = $_GET['checkin']  ?? "";
+                $checkout = $_GET['checkout'] ?? "";
+                
+                $query = "SELECT * FROM room_details";
+                
+            
+                if ($checkin !== "" && $checkout !== "") {
+                
+                    $query = "
+                        SELECT * FROM room_details 
+                        WHERE id NOT IN (
+                            SELECT room_id FROM bookings 
+                            WHERE 
+                                NOT (
+                                    checkout < '$checkin'
+                                    OR
+                                    checkin > '$checkout'
+                                )
+                        )
+                    ";
+                }
+                
+                $rooms = mysqli_query($conn, $query);
+                
 
                 while ($row = mysqli_fetch_assoc($rooms)) :
                 ?>
 
                 <div class="card mb-4 border-0 shadow overflow-hidden">
-                    <div class=" row g-0 align-items-center">
+                    <div class="row g-0 align-items-center">
 
-                        <!-- Room Image -->
 
-                        <?php 
-                                $img_path = 'admin/' . $row['main_image']; 
-                        ?>
-
-                        <div class="col-md-5 h-100">
-                            <img src="<?php echo $img_path; ?>" class="img-fluid" id="room-img">
+                        <?php $img_path = 'admin/' . $row['main_image']; ?>
+                        <div class="col-md-5">
+                            <img src="<?= $img_path; ?>" class="img-fluid">
                         </div>
 
-                        <!-- Room Info -->
+
                         <div class="col-md-5 px-3">
-                            <h5 class="mb-3"><?php echo $row['room_type']; ?></h5>
-                            <h6 class="mb-4">Tk <?php echo $row['price']; ?> per night</h6>
+                            <h5 class="mb-3"><?= $row['room_type']; ?></h5>
+                            <h6 class="mb-4">Tk
+                                <?= $row['price']; ?> per night
+                            </h6>
 
-
-                            <!-- Static Facilities -->
                             <div class="facilities mb-3">
                                 <h6>Facilities</h6>
                                 <span class="badge text-dark">Wifi</span>
@@ -92,7 +117,6 @@ require('admin/inc/connection.php');
                                 <span class="badge text-dark">AC</span>
                             </div>
 
-                            <!-- Static Guests -->
                             <div>
                                 <h6>Guests</h6>
                                 <span class="badge text-dark">2 Adults</span>
@@ -100,20 +124,18 @@ require('admin/inc/connection.php');
                             </div>
                         </div>
 
-                        <!-- Buttons -->
-                        <div class="col-md-2 pe-2 text-center">
-                            <h6 class="mb-4">Tk<?php echo $row['price']; ?></h6>
 
-                            <a href="book-now.php?id=<?php echo $row['id']; ?>"
+                        <div class="col-md-2 pe-2 text-center">
+
+                            <a href="booking_form.php?id=<?= $row['id']; ?>"
                                 class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">
-                                Book now
+                                Book Now
                             </a>
 
-                            <a href="room_details.php?id=<?php echo $row['id']; ?>"
+                            <a href="room_details.php?id=<?= $row['id']; ?>"
                                 class="btn btn-sm w-100 btn-outline-dark shadow-none">
                                 More details
                             </a>
-
                         </div>
 
                     </div>
