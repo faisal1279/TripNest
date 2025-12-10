017c1f8b348d891967537a73c16b022a98a5aa4a
Mohammad Faisal <83537344+faisal1279@users.noreply.github.com>
2025-12-10 12:35:23 +0600

    Update about.php

--- about.php
+++ about.php
@@ -3,164 +3,112 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BrightNest - About</title>
+    <title>About Us</title>
 
     <?php require('common/links.php'); ?>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
     <style>
-    .box {
-        border-top-color: #fb5200 !important;
+    .icon-box {
+        border-top: 3px solid #fb5200;
     }
     </style>
 </head>
 
-
 <body class="bg-light">
-    <!--Header -->
+
     <?php require('common/header.php'); ?>
 
+    <!-- ABOUT -->
     <div class="my-5 px-4">
-        <h2 class="fw--bold h-font text-center">Our Facilities</h2>
-        <div class="h-line bg-dark"></div>
-        <p class="text-center mt-3">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        <h2 class="fw-bold text-center">About Us</h2>
+        <div class="h-line bg-dark mb-3"></div>
 
+        <p class="text-center pb-4">
+            We provide comfortable rooms, quality service, and a peaceful environment for your perfect stay.
         </p>
+
         <div class="container">
-            <div class="row justify-content-between align-items-center">
-                <div class="col-lg-6 col-md-5 mb-5 mb-4 order-lg-1 order-md-1 order-2">
-                    <h3 class="mb-3"> Lorem Ipsum is simply dummy</h3>
-                    <p class="text-center mt-3">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
-                        type and
+            <div class="row align-items-center justify-content-between">
+
+                <div class="col-lg-6 col-md-6 mb-4">
+                    <h4>Who We Are</h4>
+                    <p>
+                        We are a modern hotel dedicated to offering good hospitality,
+                        clean rooms, and a friendly customer experience.
+                    </p>
+                    <p>
+                        Our goal is to ensure every guest feels at home with our simple,
+                        comfortable, and well-maintained facilities.
                     </p>
                 </div>
-                <div class="col-lg-5 col-md-5 mb-4 order-lg-2 order-md-2  order-1">
-                    <img src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
-                        class="w-100">
+
+                <div class="col-lg-5 col-md-6 mb-4">
+                    <img src="" class="w-100 rounded shadow-sm" n alt="">
                 </div>
+
             </div>
         </div>
     </div>
 
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-lg-3 col-md-6 mb-4 px-4">
-                <div class="bg-white rouded shadow p-4 border-top border-4 text-center box">
-                    <img src="./images/about/hotel.png" width="40px" alt="">
-                    <h4 class="mt-3">100+ ROOMs</h4>
+    <div class="container my-4">
+        <div class="row text-center">
 
+            <div class="col-md-3 mb-3">
+                <div class="bg-white p-3 shadow-sm rounded icon-box">
+                    <h5>100+ Rooms</h5>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 mb-4 px-4">
-                <div class="bg-white rouded shadow p-4 border-top border-4 text-center box">
-                    <img src="./images/about/hotel.png" width="40px" alt="">
-                    <h4 class="mt-3">200+ Customers</h4>
 
+            <div class="col-md-3 mb-3">
+                <div class="bg-white p-3 shadow-sm rounded icon-box">
+                    <h5>200+ Guests Served</h5>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 mb-4 px-4">
-                <div class="bg-white rouded shadow p-4 border-top border-4 text-center box">
-                    <img src="./images/about/hotel.png" width="40px" alt="">
-                    <h4 class="mt-3">150+ Reviews</h4>
 
+            <div class="col-md-3 mb-3">
+                <div class="bg-white p-3 shadow-sm rounded icon-box">
+                    <h5>150+ Reviews</h5>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 mb-4 px-4">
-                <div class="bg-white rouded shadow p-4 border-top border-4 text-center box">
-                    <img src="./images/about/hotel.png" width="40px" alt="">
-                    <h4 class="mt-3">100+ Stafs</h4>
 
+            <div class="col-md-3 mb-3">
+                <div class="bg-white p-3 shadow-sm rounded icon-box">
+                    <h5>50+ Staff</h5>
                 </div>
             </div>
+
         </div>
     </div>
 
-    <h3 class="my-5 fw-bold h-font text-center">Management Team</h3>
+    <!-- TEAM (Very Simple) -->
+    <h4 class="text-center my-5">Our Team</h4>
 
-    <div class="container px-4">
-        <!-- Swiper -->
-        <div class="swiper mySwiper">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
-                    <img src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
-                        class="w-100">
-                    <h5 class="mt-2">Random Name</h5>
-                </div>
-                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
-                    <img src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
-                        class="img-fluid">
-                    <h5 class="mt-2">Random Name</h5>
-                </div>
-                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
-                    <img src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
-                        class="img-fluid">
-                    <h5 class="mt-2">Random Name</h5>
-                </div>
-                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
-                    <img src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
-                        class="img-fluid">
-                    <h5 class="mt-2">Random Name</h5>
-                </div>
-                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
-                    <img src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
-                        class="img-fluid">
-                    <h5 class="mt-2">Random Name</h5>
-                </div>
+    <div class="container my-3">
+        <div class="row justify-content-center">
+
+            <div class="col-md-3 mb-4 text-center">
+                <img height="260px" src="./images/about/faisal.jpg" class="w-90 rounded shadow-sm">
+                <p class="mt-2 mb-0"><b>Mohammad Faisal</b></p>
+                <p class="text-muted small">Developer</p>
             </div>
-            <div class="swiper-button-next"></div>
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-pagination"></div>
+
+            <div class="col-md-3 mb-4 text-center">
+                <img height="260px" src="./images/about/anim.jpg" class="w-90 rounded shadow-sm">
+                <p class="mt-2 mb-0"><b>Mehrajur Rahman</b></p>
+                <p class="text-muted small">Designer</p>
+            </div>
+
+            <div class="col-md-3 mb-4 text-center">
+                <img height="260px" src="./images/about/jannat.jpg" class="w-90 rounded shadow-sm">
+                <p class="mt-2 mb-0"><b>Jannatun Nahar</b></p>
+                <p class="text-muted small">Designer</p>
+            </div>
+
         </div>
     </div>
 
-
-
-    <!-- Footer -->
-    <?php require('common/footer.php');?>
-
-
-    <!-- Swiper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
-    <!-- Initialize Swiper -->
-    <script>
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        keyboard: {
-            enabled: true,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-            },
-            640: {
-                slidesPerView: 1,
-            },
-            768: {
-                slidesPerView: 3,
-            },
-            1024: {
-                slidesPerView: 4,
-            },
-        }
-    });
-    </script>
+    <?php require('common/footer.php'); ?>
 
 </body>
 
