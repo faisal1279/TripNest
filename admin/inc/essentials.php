5a76ef0f4093e2012a6a41b772810434411e1739
Mohammad Faisal <83537344+faisal1279@users.noreply.github.com>
2025-12-10 12:10:17 +0600

    Update essentials.php

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -1,26 +1,13 @@
 <?php
+
+session_start();  
+
 function adminLogin()
 {
-    session_start();
-
-    if(!isset($_SESSION['adminLogin'])){
-        echo "<script>window.location.href='index.php';</script>";
+    if (empty($_SESSION['adminLogin'])) {
+        header("Location: index.php");
         exit;
     }
 }
 
-function redirect($url){
-    echo "<script>window.location.href='$url';</script>";
-}
-
-function alert($type, $msg){
-    $cls = ($type == "success") ? "alert-success" : "alert-danger";
-
-    echo "
-        <div class='alert $cls alert-dismissible fade show' role='alert'>
-            $msg
-            <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-        </div>
-    ";
-}
 ?>
