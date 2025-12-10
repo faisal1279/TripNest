06b5991c035f477cc54d93e85e7fe49af3ef6557
Mohammad Faisal <83537344+faisal1279@users.noreply.github.com>
2025-12-10 12:11:00 +0600

    Update connection.php

--- admin/inc/connection.php
+++ admin/inc/connection.php
@@ -2,6 +2,6 @@
 $conn = mysqli_connect("localhost:3307", "root", "", "hootel_bookin");
 
 if(!$conn){
-    die("Database Connection Failed: " . mysqli_connect_error());
+    die("Database Connection Failed: ");
 }
 ?>
